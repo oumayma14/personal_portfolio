003c7124075042dc8e1ae12ba3684d321fa528b1
oumayma14 <oumaima0fazani@gmail.com>
2025-11-28 02:45:37 +0100

    finish the stars background

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+export const StarBackground = () =>{
+    const [stars, setStars] = useState([]);
+    useEffect(()=>{
+        generateStars();
+    }, []);
+
+    const generateStars = () => {
+        const numberOfStars = Math.floor(
+            (window.innerWidth * window.innerHeight)/10000
+        );
+        const newStars = []
+        for (let i = 0; i< numberOfStars; i++){
+            newStars.push({
+                id:i,
+                size: Math.random() * 3 + 1,
+                x: Math.random() * 100,
+                y: Math.random() * 100,
+                opacity: Math.random() * 4+2,,
+                animationDuration: Math.random() * 4 + 2,
+            })
+        }
+
+        setStars(newStars);
+    };
+    return <div className="fixed inset-0 overflow-hidden pointer-evenets-none z-0">
+        {stars.map((star) => (
+            <div key={star.id} className="star">
+        )) }
+    </div>
+};

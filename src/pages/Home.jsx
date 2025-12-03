d67d1678b057fab4556c0b22893e9634ff5a48d8
oumayma14 <oumaima0fazani@gmail.com>
2025-12-03 22:55:35 +0100

    skills added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutMe } from "../components/AboutMe";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBAckground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -14,6 +15,7 @@ export const Home = () =>{
         <main>
             <HeroSection />
             <AboutMe/>
+            <SkillsSection />
         </main>
     </div>    
     );
